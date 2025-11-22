375d7a171617ca70d2042188d9357524283f6e37
Julio M Cruz <julio.cruz@eb-ms.net>
2025-11-21 23:40:37 -0300

    initial commit

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,61 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+import { Navbar } from '@/components/navbar';
+import Providers from "@/components/providers"
+
+const inter = Inter({ subsets: ['latin'] });
+
+const appUrl = process.env.NEXT_PUBLIC_URL || "http://localhost:3000";
+
+// Embed metadata for Farcaster sharing
+const frame = {
+  version: "1",
+  imageUrl: `${appUrl}/opengraph-image.png`,
+  button: {
+    title: "Launch my-celo-app",
+    action: {
+      type: "launch_frame",
+      name: "my-celo-app",
+      url: appUrl,
+      splashImageUrl: `${appUrl}/icon.png`,
+      splashBackgroundColor: "#ffffff",
+    },
+  },
+};
+
+export const metadata: Metadata = {
+  title: 'my-celo-app',
+  description: 'A new Celo blockchain project',
+  openGraph: {
+    title: 'my-celo-app',
+    description: 'A new Celo blockchain project',
+    images: [`${appUrl}/opengraph-image.png`],
+  },
+  other: {
+    "fc:frame": JSON.stringify(frame),
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {/* Navbar is included on all pages */}
+        <div className="relative flex min-h-screen flex-col">
+          <Providers>
+            <Navbar />
+            <main className="flex-1">
+              {children}
+            </main>
+          </Providers>
+        </div>
+      </body>
+    </html>
+  );
+}
