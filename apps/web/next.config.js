eacd8df14c2ad618398ed9d7b4d6e1dedc2b13eb
Julio M Cruz <julio.cruz@eb-ms.net>
2025-11-22 07:37:57 -0300

    fixing framework nextjs

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,8 +5,6 @@ const nextConfig = {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
   },
-  // Disable Turbopack for now - use Webpack instead
-  // The experimental.turbo config is removed to allow Webpack to work
 };
 
 module.exports = nextConfig;
